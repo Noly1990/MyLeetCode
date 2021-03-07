af1f90f9c23a16f34d78068d4835b54669c6efb6
noly-pro13 <noly@pro13.com>
2021-03-08 00:49:27 +0800

    save 20210308

--- others/q503.ts
+++ others/q503.ts
@@ -1,5 +1,41 @@
 // 503. 下一个更大元素 II https://leetcode-cn.com/problems/next-greater-element-ii/
 
+
 function nextGreaterElements(nums: number[]): number[] {
-    
+    let stack = [0];
+    let ans = new Array(nums.length).fill(-1)
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[stack[0]]) {
+            while (stack.length > 0 && nums[i] > nums[stack[0]]) {
+                let one = stack.shift();
+                if (nums[i] > nums[one]) {
+                    ans[one] = nums[i]
+                }
+            }
+
+        }
+        stack.unshift(i)
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > nums[stack[0]]) {
+            while (stack.length > 0 && nums[i] > nums[stack[0]]) {
+                let one = stack.shift();
+                if (nums[i] > nums[one]) {
+                    ans[one] = nums[i]
+                }
+            }
+
+        }
+        stack.unshift(i)
+    }
+
+    return ans;
+
 };
+
+
+
+
+
+
+console.log(nextGreaterElements([1, 9, 2, 1, 6, 6, 9, 4, 3, 5, 7, 2, 9, 11, 6, 5, 4, 3, 2]))
